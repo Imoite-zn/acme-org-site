d1423358867bf92aa6901a77e0b7fb47c42420bf
Imoite-zn <150998266+Imoite-zn@users.noreply.github.com>
2024-03-07 03:15:51 +0300

    Update Header.js
    logo size to 75w

--- components/common/Header.js
+++ components/common/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
         <div className='container'>
           <div className='logo'>
             <Link href='/'>
-              <img src="/images/new-logo.jpg" width={'60'} height={'60'}></img>
+              <img src="/images/new-logo.jpg" width={'75'} height={'60'}></img>
             </Link>
           </div>
           <nav className={open ? "openMenu" : "closeMenu"} onClick={() => setOpen(null)}>
