28ba6c58e62488f8c00e8b709b0bbb7579772af8
Imoite-zn <150998266+Imoite-zn@users.noreply.github.com>
2024-04-19 14:00:06 +0300

    Update [profiles].js

--- pages/profiles/[profiles].js
+++ pages/profiles/[profiles].js
@@ -1,14 +1,14 @@
-import { blogdata } from "@/assets/data/dummydata"
+import { teamdata } from "@/assets/data/dummydata"
 import Banner from "@/components/Banner"
 import { Title, TitleSm } from "@/components/common/Title"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import React from "react"
 
-const SinglePost = () => {
+const SingleProfile = () => {
   const router = useRouter()
   const { id } = router.query
-  const post = blogdata.find((post) => post.id === parseInt(id))
+  const post = teamdata.find((post) => post.id === parseInt(id))
   
   if (!post) {
     return <div>Loading...</div>;
@@ -22,7 +22,7 @@ const SinglePost = () => {
       <section className='post-details bg-top'>
         <div className='container'>
           <div className='heading-title'>
-            <TitleSm title='Strategies & Approaches' /> <br />
+            <TitleSm title='Our Team' /> <br />
             <br />
             <Title title={post.title} className='title-bg' />
             <div className='img py'>
@@ -36,21 +36,10 @@ const SinglePost = () => {
             </div>
           </div>
           <Banner />
-
-          <div className='heading-title'>
-            <div className='desc'>
-              <TitleSm title='Integer a justo vitae arcu fermentum...' />
-
-              <p className='desc-p'> Phasellus nec tempor neque. In nec finibus lorem, in aliquet risus. Proin elit elit, cursus vel vulputate at, volutpat quis metus. Praesent at blandit tellus.</p>
-              <p className='desc-p'>Morbi finibus velit erat, a pulvinar lacus mollis sit amet. Nulla iaculis convallis fermentum. Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper! Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
-              <p className='desc-p'>Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper.</p>
-              <p className='desc-p'>Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
-            </div>
-          </div>
         </div>
       </section>
     </>
   )
 }
 
-export default SinglePost
+export default SingleProfile
