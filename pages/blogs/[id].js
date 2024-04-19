f1a7cde2fd126d52ad0611f568d8acd186cbdd49
Imoite-zn <150998266+Imoite-zn@users.noreply.github.com>
2024-04-19 16:20:18 +0300

    Update [id].js

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -36,18 +36,8 @@ const SinglePost = () => {
             </div>
           </div>
           <Banner />
-
-          <div className='heading-title'>
-            <div className='desc'>
-              <TitleSm title='Integer a justo vitae arcu fermentum...' />
-
-              <p className='desc-p'> Phasellus nec tempor neque. In nec finibus lorem, in aliquet risus. Proin elit elit, cursus vel vulputate at, volutpat quis metus. Praesent at blandit tellus.</p>
-              <p className='desc-p'>Morbi finibus velit erat, a pulvinar lacus mollis sit amet. Nulla iaculis convallis fermentum. Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper! Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
-              <p className='desc-p'>Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper.</p>
-              <p className='desc-p'>Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
-            </div>
-          </div>
         </div>
+        <br />
       </section>
     </>
   )
