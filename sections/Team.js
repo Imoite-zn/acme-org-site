abb3069d571f1b792f3ef22230e17dd1a773876a
Imoite-zn <150998266+Imoite-zn@users.noreply.github.com>
2024-04-19 13:55:46 +0300

    Update Team.js

--- sections/Team.js
+++ sections/Team.js
@@ -16,7 +16,7 @@ const Team = () => {
           </div>
           <div className='grid-4 py'>
             {teamdata.map((item) => (
-              <Card data={item} key={item.id} caption={item.post} />
+              <Card data={item} key={item.id} caption={item.post} path='profiles' />
             ))}
           </div>
         </div>
