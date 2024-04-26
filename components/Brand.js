4a9d13d7e6411b8deeb7d4b5aec4d5474626fce0
Imoite-zn <150998266+Imoite-zn@users.noreply.github.com>
2024-04-26 07:42:38 +0300

    Update Brand.js

--- components/Brand.js
+++ components/Brand.js
@@ -5,8 +5,17 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+
 const Brand = () => {
 
+  const settings =  {
+    dots: true,
+    infinite: true,
+    speed: 500,    
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+    
   return (
     <>
       <section className='brand'>
@@ -14,14 +23,14 @@ const Brand = () => {
          <div className='heading-title'>
             <Title title='OUR PARTNERS' />
           </div>
-          <div className='brand-content grid-6 py'>
-        
-         {brand.map((item) => (
+    <Slider {...settings}>
+          <div className='brand-content  py'>
+           {brand.map((item) => (
               <div className='images' key={item.id}>
                 <img src={item.cover} alt={item.id} width='100%' height='auto' />
               </div>
             ))}
-            
+    </Slider>       
           </div>
         </div>
       </section> 
