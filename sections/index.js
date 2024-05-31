614913790ac271e3643dbb3b4f0c2179de89fccc
Imoite-zn <lawrenceimoite@gmail.com>
2024-05-31 09:16:50 -0500

    new on blogs

--- sections/index.js
+++ sections/index.js
@@ -2,8 +2,8 @@ import Hero from "./Hero"
 import Agency from "./Agency"
 import ShowCase from "./ShowCase"
 import Team from "./Team"
-import Blogs from "./Blogs"
+import Blog from "./Blog"
 import Contact from "./Contact"
 import Members from "./Members"
 
-export { Hero, Agency, Team, ShowCase, Blogs, Contact, Members }
+export { Hero, Agency, Team, ShowCase, Blog, Contact, Members }
